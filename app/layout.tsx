1ae0145ab43c9ec391ef20cd6f882b585800b54f
Neil Sen Easow <neilseneasow@gmail.com>
2024-12-14 17:03:43 +0530

    Making changes to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <body>{children}</body> */}
-      {/* This is a test line, please ignore*/}
+      
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
